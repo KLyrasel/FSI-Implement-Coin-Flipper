d34ac00da3206a6c02ea8b3ad3f69a1b669296f8
Kai Morris <107083617+KLyrasel@users.noreply.github.com>
2022-07-12 21:21:51 -0400

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,41 @@
 // TODO: Declare any global variables we need
-
-
+let heads = 0
+let tails = 0
+let percentHeads = 0
+let percentTails = 0
 document.addEventListener('DOMContentLoaded', function () {
     // This is just a sanity check to make sure your JavaScript script is getting loaded
     // You can remove it once you see it in your browser console in the developer tools
     console.log('Hi')
+
+    document.getElementById("flip").addEventListener("click",function(){
+  let flippedHeads =  Math.random()<0.5
+  console.log(flippedHeads)
+  if(flippedHeads){
+  document.getElementById("penny").src="assets/images/penny-heads.jpg"
+  document.getElementById("message").textContent = "You Flipped Heads"
+  heads ++
+}
+else {
+    document.getElementById("penny").src="assets/images/penny-tails.jpg"
+    document.getElementById("message").textContent = "You Flipped Tails"
+    tails ++
+}
+total()
+    })
+    
+
+    document.getElementById("clear").addEventListener("click",function(){
+    console.log("clear")
+    heads = 0
+    tails = 0
+    percentHeads = 0
+    percentTails = 0
+   let total = heads + tails
+
+   display()
+
+    })
 
     // TODO: Add event listener and handler for flip and clear buttons
 
@@ -25,3 +56,20 @@ document.addEventListener('DOMContentLoaded', function () {
         // TODO: Update the scoreboard (same logic as in flip button click handler)
 
 })
+function total (){
+let total = heads + tails 
+console.log(total) 
+
+
+if (total >0) {
+    percentHeads = Math.round((heads/total) * 100) 
+    percentTails = Math.round((tails/total)* 100)
+}
+display()
+}
+function display(){
+    document.getElementById("heads").textContent = heads
+document.getElementById("heads-percent").textContent = percentHeads + "%"
+document.getElementById("tails").textContent = tails
+document.getElementById("tails-percent").textContent = percentTails + "%"
+}
